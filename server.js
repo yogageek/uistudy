63956b6b345cb2985e67992b1899459bf424d478
yogageek <hsiaoyoga@gmail.com>
2021-08-29 08:52:08 +0000

    uistudy week 3 start

--- server.js
+++ server.js
@@ -6,6 +6,7 @@
 'use strict';
 
 const express = require('express');
+const fs = require('fs');
 
 // Constants
 const PORT = 3000;
@@ -13,9 +14,27 @@ const HOST = '0.0.0.0';
 
 // App
 const app = express();
-app.get('/', (req, res) => {
-	res.send('Hello remote world!\n');
+// app.get('/', (req, res) => {
+// 	// res.send('Hello remote world yoga!\n');
+// });
+app.get("/", function(req, res) {
+    res.sendfile(__dirname + '/index.html', function(err) {
+        if (err) res.send(404);
+    });
+});
+
+app.get("/list_movies", (req, res) => {
+	fs.readFile(__dirname + '/' + 'movies.json', 'utf8', (err, data) => {
+		res.end(data);
+	});
 });
 
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
+
+
+// app = express.createServer(express.logger());
+// port = process.env.PORT || 3000;
+// app.listen(port, function() {
+//     console.log("Listening on " + port);
+// });
